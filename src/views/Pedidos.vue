4ae4b623c49ef761366e896f9c6fe94e8322dd09
Matheus-Imberio <matheusimberio030@gmail.com>
2023-10-23 20:26:24 -0300

    firste commit

---
+++ src/views/Pedidos.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main-container">
+    <h1>Gerenciar Pedidos:</h1>
+    <Dashboards/>
+  </div>
+</template>
+<script>
+import Dashboards from '@/components/Dashboards.vue';
+
+export default {
+    name: "Pedidos",
+    components: { Dashboards }
+}
+</script>
+<style scoped>
+
+</style>
